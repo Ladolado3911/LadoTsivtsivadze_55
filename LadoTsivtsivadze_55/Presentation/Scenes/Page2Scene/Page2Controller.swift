fe0d7b0ef9ab9818b0afadbf6fd1e57949593b0b
lado at luka <tsivtsivadzelado@yahoo.com>
2021-07-16 08:21:03 -0700

    naming groups and adding coordinator

---
+++ LadoTsivtsivadze_55/Presentation/Scenes/Page2Scene/Page2Controller.swift
@@ -0,0 +1,29 @@
+//
+//  Page2Controller.swift
+//  LadoTsivtsivadze_55
+//
+//  Created by lado tsivtsivadze on 7/16/21.
+//
+
+import UIKit
+
+class Page2Controller: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
